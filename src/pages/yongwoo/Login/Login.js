92e2ba0e3597c6cdca7d7de1ee4020df7d36095d
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2021-11-17 20:04:31 +0900

    Add : 초기 설정

--- src/pages/yongwoo/Login/Login.js
+++ src/pages/yongwoo/Login/Login.js
@@ -1,6 +1,92 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function Logo() {
+  return <h1 className="logo">Westagram</h1>;
+}
+
+function UserInfo() {
+  return (
+    <>
+      <input
+        className="userName"
+        type="text"
+        placeholder="전화번호, 사용자 이름 또는 이메일"
+      />
+      <input className="userPassword" type="password" placeholder="비밀번호" />
+    </>
+  );
+}
+
+function SignUpWrap() {
+  return (
+    <article className="signUpWrap">
+      <p>
+        계정이 없으신가요?{" "}
+        <Link to="/Main">
+          <button href="" className="signUpBlue">
+            가입하기
+          </button>
+        </Link>
+      </p>
+    </article>
+  );
+}
+
+function LoginButton() {
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate("/Main");
+  };
+  return (
+    <button className="loginButton" onClick={goToMain}>
+      로그인
+    </button>
+  );
+}
+
+function StoreWrap() {
+  return (
+    <>
+      <article className="storeWrap">
+        <img
+          src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_korean-ko.png/4a5c9d62d51b.png"
+          alt="appStore"
+        />
+        <img
+          src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_korean-ko.png/f155b664a93b.png"
+          alt="googlePlay"
+        />
+      </article>
+    </>
+  );
+}
+
 function LoginYongwoo() {
   return (
-    <div>하이</div>
-  )
+    <>
+      <section className="login">
+        <article className="topWrap">
+          <Logo />
+          <UserInfo />
+          <LoginButton />
+          <div className="meddleLine">
+            <div className="line"></div>
+            <p>또는</p>
+            <div className="line"></div>
+          </div>
+          <div className="facebookWrap">
+            <p>Facebook으로 로그인</p>
+          </div>
+          <p className="passwordLost">비밀번호를 잊으셨나요?</p>
+        </article>
+        <SignUpWrap />
+        <p className="downlodeCopyright">앱을 다운로드하세요.</p>
+        <StoreWrap />
+      </section>
+    </>
+  );
 }
+
 export default LoginYongwoo
--- src/pages/yongwoo/Main/Main.js
+++ src/pages/yongwoo/Main/Main.js
@@ -1,6 +1,281 @@
+import React from "react";
+
+function LogoWrap(params) {
+  return (
+    <article className="logoWrap">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/1384/1384031.png"
+        alt="로고"
+      />
+      <div className="columnLine"></div>
+      <h1 className="logo">Westagram</h1>
+    </article>
+  );
+}
+
+function Search() {
+  return (
+    <article className="search">
+      <input type="text" placeholder="검색" />
+    </article>
+  );
+}
+
+function PictogramWrap() {
+  return (
+    <article className="pictogramWrap">
+      <img
+        src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+        alt="탐색"
+      />
+      <img
+        src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+        alt="하트"
+      />
+      <img
+        src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+        alt="프로필"
+      />
+    </article>
+  );
+}
+
+function Navigation() {
+  return (
+    <>
+      <nav className="navigation">
+        <div className="navContainer">
+          <LogoWrap />
+          <Search />
+          <PictogramWrap />
+        </div>
+      </nav>
+    </>
+  );
+}
+
+function ProfileWrap(params) {
+  return (
+    <div className="profileWrap">
+      <div className="logoWrap">
+        <img src="/images/profile.jpeg" alt="logo" />
+        <h2>94_yongyong_lee</h2>
+      </div>
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+function FeedsPictogram() {
+  return (
+    <div className="feedsPictogram">
+      <div className="feedsPictogramLeft">
+        <img src="/images/heart.png" alt="하트" />
+        <img
+          className="speachBubble"
+          src="/images/bubble-chat.png"
+          alt="speachBubble"
+        />
+        <img src="/images/share.png" alt="share" />
+      </div>
+      <img
+        className="saveIcon"
+        src="https://cdn-icons-png.flaticon.com/512/84/84510.png"
+        alt="save"
+      />
+    </div>
+  );
+}
+
+function CommentContainer() {
+  return (
+    <div className="commentContainer">
+      <div className="commentWrap">
+        <input type="text" placeholder="댓글달기..." />
+        <button className="commetBtn">게시</button>
+      </div>
+    </div>
+  );
+}
+
+function PhotoWrap(params) {
+  return (
+    <div className="photoWrap">
+      <img src="/images/instagram_feed.jpeg" alt="mainPhoto" />
+    </div>
+  );
+}
+
+function GoodWrap() {
+  return (
+    <div className="goodWrap">
+      <img className="goodImg" src="/images/goodImage.jpeg" alt="" />
+      <p>sung_sae_h2 님 외 24명이 좋아합니다.</p>
+    </div>
+  );
+}
+
+function TextWrap() {
+  return (
+    <div className="textWrap">
+      <p className="text">
+        #모동숲 #모여봐요동물의숲 #박새로이머리 #오징어 #낚시
+      </p>
+      <div className="comment">
+        <p className="commentNameBold">
+          jeffkim01
+          <span className="commentText"> 진짜 세상 부럽다... 저도 동숲...</span>
+        </p>
+        <i className="fas fa-heart textolorHeart"></i>
+      </div>
+      <div className="commentStart">
+        <i className="far fa-trash-alt"></i>
+        <i className="far fa-heart"></i>
+        <i className="fas fa-heart colorHeart"></i>
+      </div>
+      <p className="commentView">54분전</p>
+    </div>
+  );
+}
+
+function FeedsContainer() {
+  return (
+    <main className="feedsContainer">
+      <article className="feed">
+        <ProfileWrap />
+        <PhotoWrap />
+        <FeedsPictogram />
+        <GoodWrap />
+        <TextWrap />
+        <CommentContainer />
+      </article>
+    </main>
+  );
+}
+function RecommendContainer() {
+  return (
+    <main className="recommendContainer">
+      <article className="mainRight">
+        <div className="myInfoWrap">
+          <div className="myInfoPhotoWrap">
+            <img src="/images/profile.jpeg" alt="프로필" />
+            <div className="myInfo">
+              <h2>94_yongyong_lee</h2>
+              <p>이용우</p>
+            </div>
+          </div>
+          <p className="infoBlue">전환</p>
+        </div>
+        <div className="recommendTitle">
+          <p className="grayColor">회원님을 위한 추천</p>
+          <p className="blackColor">모두 보기</p>
+        </div>
+        <div className="recommendWrap">
+          <div className="recommend">
+            <img
+              className="recommendPhoto"
+              src="/images/recommend_01.jpeg"
+              alt="추천1"
+            />
+            <div className="recommendText">
+              <h4>jomingi62</h4>
+              <p>dlfjwk11003님이 팔로우합니다</p>
+            </div>
+          </div>
+          <div className="follow">팔로우</div>
+        </div>
+
+        <div className="recommendWrap">
+          <div className="recommend">
+            <img
+              className="recommendPhoto"
+              src="/images/recommend_02.jpeg"
+              alt="추천2"
+            />
+            <div className="recommendText">
+              <h4>s_._hoo</h4>
+              <p>회원님을 팔로우합니다</p>
+            </div>
+          </div>
+          <div className="follow">팔로우</div>
+        </div>
+
+        <div className="recommendWrap">
+          <div className="recommend">
+            <img
+              className="recommendPhoto"
+              src="/images/recommend_03.jpeg"
+              alt="추천3"
+            />
+            <div className="recommendText">
+              <h4>s_._hoo</h4>
+              <p>회원님을 팔로우합니다</p>
+            </div>
+          </div>
+          <div className="follow">팔로우</div>
+        </div>
+
+        <div className="recommendWrap">
+          <div className="recommend">
+            <img
+              className="recommendPhoto"
+              src="/images/recommend_04.jpeg"
+              alt="추천4"
+            />
+            <div className="recommendText">
+              <h4>s_._hoo</h4>
+              <p>회원님을 팔로우합니다</p>
+            </div>
+          </div>
+          <div className="follow">팔로우</div>
+        </div>
+
+        <div className="recommendWrap">
+          <div className="recommend">
+            <img
+              className="recommendPhoto"
+              src="/images/recommend_05.jpeg"
+              alt="추천5"
+            />
+            <div className="recommendText">
+              <h4>s_._hoo</h4>
+              <p>회원님을 팔로우합니다</p>
+            </div>
+          </div>
+          <div className="follow">팔로우</div>
+        </div>
+
+        <div className="infomation">
+          <ul className="infomatioinList">
+            <li>소개.</li>
+            <li>도움말.</li>
+            <li>홍보센터.</li>
+            <li>API.</li>
+            <li>채용정보.</li>
+            <li>개인정보처리방침.</li>
+            <li>약관.</li>
+            <li>위치.</li>
+            <li>인기 계정.</li>
+            <li>해시태그.</li>
+            <li>언어</li>
+          </ul>
+          <p>© 2021 INSTAGRAM FROM META</p>
+        </div>
+      </article>
+    </main>
+  );
+}
+
 function MainYongwoo() {
   return (
-    <div>하이</div>
-  )
+    <>
+      <Navigation />
+      <div className="main">
+        <FeedsContainer />
+        <RecommendContainer />
+      </div>
+    </>
+  );
 }
-export default MainYongwoo
+
+export default MainYongwoo;
