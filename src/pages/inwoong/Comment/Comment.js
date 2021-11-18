012b844c98904172aeabf0a4b869e6166cab0edf
이누인웅's air <macbookair@inu-in-ung.local>
2021-11-18 21:52:29 +0900

    Modify: map key값에 카운터 부여

--- src/pages/inwoong/Comment/Comment.js
+++ src/pages/inwoong/Comment/Comment.js
@@ -1,9 +1,10 @@
 import './Comment.scss';
 function Comments({ comments }) {
+  let counter = 1;
   return (
     <div>
-      {comments.map((el, index) => (
-        <div className="comment" key={index}>
+      {comments.map(el => (
+        <div className="comment" key={counter++}>
           <span className="likeComment bold commentAdd">cannon_mj</span>
           <span className=" likeComment dot">{el} </span>
         </div>
