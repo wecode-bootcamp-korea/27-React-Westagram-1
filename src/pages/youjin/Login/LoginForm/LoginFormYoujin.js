56dc342dc7f6712e8feab9eb11023bf85023ede7
sodalite1204 <your.jiny.9@gmail.com>
2021-11-19 21:51:45 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/youjin/Login/LoginForm/LoginFormYoujin.js
+++ src/pages/youjin/Login/LoginForm/LoginFormYoujin.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginBtnYoujin from './LoginBtnYoujin';
 import './LoginFormYoujin.scss';
 
 function LoginFormYoujin() {
+  const [isValue, setIsValue] = useState({
+    idValue: '',
+    passwordValue: '',
+  });
+
+  const handleIdInput = event => {
+    const { name, value } = event.target;
+    setIsValue({
+      ...isValue,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="loginFormYoujin">
       <form>
@@ -10,8 +23,16 @@ function LoginFormYoujin() {
           id="id1"
           type="text"
           placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={handleIdInput}
+          name="idValue"
         />
-        <input id="password1" type="password" placeholder="비밀번호" />
+        <input
+          id="password1"
+          type="password"
+          placeholder="비밀번호"
+          onChange={handleIdInput}
+          name="passwordValue"
+        />
         <LoginBtnYoujin />
       </form>
     </div>
